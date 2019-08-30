bf9fbdd0bf710ffc17059ac289290580a05e9c84
Leonardo Manzella <LeonardoManzella@users.noreply.github.com>
2019-08-30 00:57:23 -0300

    Updating estado_transito.js

--- estado_transito.js
+++ estado_transito.js
@@ -1,63 +1,55 @@
-
 var Xray = require('x-ray');
 var xray_ready = Xray();
 var request = require('request');
 var cheerio = require("cheerio");
-var value_change = "0,13";
-var date_change = 0;   //Sunday is 0, Monday is 1, and so on
-var date_limit = 15;
 
-function extract_number(text){
-    return text.replace("-","").replace("%",""); 
+function extract_status(original_string){
+  console.log("Extracting Status from " + original_string + "..");
+  var new_string = "";
+  var regular_expresion = /Retiro(\w)*Jos/i;      //Obtain status for José León Suárez-Retiro. It does repeats endlesly "José León Suárez-Retiro<status>José León Suárez-Retiro"
+  var first_word = "Retiro";
+  var second_word = "Jos";
+  new_string = original_string.match(regular_expresion)[0];   //Obtain first ocurrence
+  new_string = new_string.slice(first_word.length  , new_string.search(second_word) );    //Slicing innecesary text
+  console.log("Status: " + JSON.stringify(new_string));
+  
+  return new_string;
 }
 
-function hasMinus(text){
-    return (text.indexOf("-")>-1)
-}
-
-function hasChanges(data){
-  var new_date = new Date();
-  var actual_date = new_date.getDay();
+function is_normal(an_status){
   
-  //Only let pass if Month Day is between 0 and date_limit
-  if( new_date.getUTCDate() > date_limit ){
-    return false;
-  }
-  
-  
-  //If exists a big difference or today is the change date
-  if( (extract_number(data[0].difference)>=value_change) || (extract_number(data[1].difference)>=value_change) || actual_date==date_change ){
+  if( an_status=='Normal' ){
     return true;
   }
-  
   return false;
 }
 
-function isUp(difference){
-  return (extract_number(difference)>=value_change && !hasMinus(difference) )
+//Highlights Lines with unexpected status
+function status_message(an_status){
   
+  if( is_normal(an_status) ){
+    return an_status;
+  }
+  
+  return '<big style="background-color:#ff5e5e;">&nbsp;&nbsp;' + an_status + '&nbsp;&nbsp;</big>';
 }
 
-function isDown(difference){
-  return (extract_number(difference)>=value_change && hasMinus(difference) )
+function hasChanges(data){
   
-}
-
-function make_message(data){
-  if( isUp(data[0].difference) ){
-    return "!! Dolar Oficial Subiendo " + data[0].difference + " esta " + data[0].buy_price
-  }
-  if( isDown(data[0].difference) ){
-    return "!! Dolar Oficial Bajando " + data[0].difference + " esta " + data[0].buy_price
-  }
-  if( isUp(data[1].difference) ){
-    return "!! Dolar Informal Subiendo " + data[1].difference + " esta " + data[0].buy_price
-  }
-  if( isDown(data[1].difference) ){
-    return "!! Dolar Informal Bajando " + data[1].difference + " esta " + data[0].buy_price
+  //False ONLY if all lines are normal
+  if( is_normal(data.status_line_a) &&
+      is_normal(data.status_line_b) && 
+      is_normal(data.status_line_c) && 
+      is_normal(data.status_line_d) && 
+      is_normal(data.status_line_e) && 
+      is_normal(data.status_line_h) && 
+      is_normal(data.status_urquiza) &&
+      is_normal(data.status_mitre) ){
+    return false;
   }
   
-  return "Dolar Comp Informal " + data[1].buy_price
+  //ONLY True if some lines is not normal
+  return true;
 }
 
 function sendToMaker(makerKey,eventName,data){
@@ -65,18 +57,17 @@ function sendToMaker(makerKey,eventName,data){
   console.log("URL " + url_string);
   if( hasChanges(data) ){
   //if( true ){
-  var html = '<table style="width:100%; background-color:powderblue; font-size: 180%"> <tr style="width:100%; background-color:powderblue;"> <td><center><b><big>PRECIO DOLAR</big></b</center></td> </tr> </table> <table style="width:100%; background-color:powderblue; font-size: 140%"> <tr style="width:50%"> <td><center><b><big>' + data[0].title + '</big></b></center></td> <td><center><b><big>' + data[1].title + '</big></b></center></td> </tr> <tr style="width:50%"> <td><center><b><big>' + data[0].difference + '</big></b></center></td> <td><center><b><big>' + data[1].difference + '</big></b></center></td> </tr><tr style="width:50%"> <td><center><b><big>' + data[0].buy_price + '</big></b></center></td> <td><center><b><big>' + data[1].buy_price + '</big></b></center></td> </tr> </table>';
-  var message = make_message(data);
-  var d = new Date();
-  var actual_date = d.getDate().toString() + "/" + (d.getMonth()+1).toString();
-  console.log("actual date " + actual_date);
+  var html = '<table style="width:100%; background-color:#b2b2b2; font-size: 180%"> <tr style="width:100%; background-color:#b2b2b2;"><td><center><b><big>ESTADO TRANSPORTE</big></b</center></td> </tr> </table> <table style="width:100%; background-color:#b2b2b2; font-size: 140%"> <tr style="width:50%"> <td><center><b><big>Linea</big></b></center></td> <td><center><b><big>Estado</big></b></center></td> </tr> <tr style="width:50%"><td><center><b><big style="background-color:#ff6b6b;">&nbsp;&nbsp;Linea B&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_b) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#4a82f9;">&nbsp;&nbsp;Linea C&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_c) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#13ad53;">&nbsp;&nbsp;Linea D&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_d) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#6acbff;">&nbsp;&nbsp;Linea A&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_a) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#ab5eff;">&nbsp;&nbsp;Linea E&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_e) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#ffee5e;">&nbsp;&nbsp;Linea H&nbsp;&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_line_h) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#ff993a;">&nbsp;Tren Urq&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_urquiza) + '</big></b></center></td> </tr><tr style="width:50%"><td><center><b><big style="background-color:#eff5ff;">&nbsp;Tren Mitre&nbsp;</big></b></center></td><td><center><b><big>' + status_message(data.status_mitre) + '</big></b></center></td> </tr></table>';
+    var d = new Date();
+    var actual_date = d.getDate().toString() + "/" + (d.getMonth()+1).toString();
+    console.log("actual date " + actual_date);
   
   request.post({
       url: url_string,
       form:    { 
     'value1' : html,
-    'value2' : message,
-    'value3' : actual_date}
+    'value2' : actual_date,
+    'value3' : ""}
     }, function(error, response, body) {
       console.log('Body response was ', body);
       console.log('Error was ', error);
@@ -93,13 +84,17 @@ module.exports =
     res.writeHead(200, { 'Content-Type': 'application/json' });
     //writeJSON(res);
     console.log("Xray Begin");
-    xray_ready('http://www.ambito.com/economia/mercados/monedas/dolar/', {data: xray_ready(
-      '.container-fluid div section div div', [{
-        title: 'div div h2 a b',
-        difference: 'div div div .variacion big',
-        buy_price: 'div div div .cierreAnterior big'
-        }]
-      )
+    xray_ready('http://www.metrovias.com.ar/', {data: xray_ready(
+        '#linesStateSection', [{
+          status_line_a: '#status-line-A',
+          status_line_b: '#status-line-B',
+          status_line_c: '#status-line-C',
+          status_line_d: '#status-line-D',
+          status_line_e: '#status-line-E',
+          status_line_h: '#status-line-H',
+          status_urquiza: '#status-line-U'
+          }]
+        )
     })(function(err, data) {
     console.log("Xray End")
     
@@ -109,24 +104,41 @@ module.exports =
       res.write( JSON.stringify(err));
     }
     else{
-      console.log("Writing Response")
-      var realData = [
-          data.data[1],
-          data.data[2]
-        ];
-       console.log("Real Data " + JSON.stringify( realData ))
+      console.log("Inner Xray Begin");
+      xray_ready('http://servicios.lanacion.com.ar/transito', {extra_data: xray_ready(
+        '.trenes #ramales', [{
+          raw_line: ' .linea3'
+          }]
+        )
+      })(function(err, extra_data) {
+      console.log("Inner Xray End")
       
-      console.log("Sending event to Maker...")
-      var leo_token = "BSBTCmAfGhaWR30dBE3oP";
-      var event_name = "dolar_changed";
-      sendToMaker(leo_token,event_name,realData);
-      console.log("Event send!")
-      res.write( JSON.stringify( realData ));
-    }
-    
-    res.end();
-    console.log("Response End")
-    })
+      
+      if(err){
+        console.log("Inner Xray Error! " + JSON.stringify(err))
+        res.write( JSON.stringify(err));
+      }
+      else{
+         console.log("Data " + JSON.stringify( data ))
+         console.log("Extra Data " + JSON.stringify( extra_data ))
+        
+        console.log("Writing Response")
+        var realData = data.data[0];
+        realData.status_mitre = extract_status(extra_data.extra_data[0].raw_line);
+        console.log("Real Data " + JSON.stringify( realData ))
+        
+        console.log("Sending event to Maker...")
+        var leo_token = "bEJDjvRQ04PPYZVlKAP2E8";
+        var event_name = "estado_transito";
+        sendToMaker(leo_token,event_name, realData);
+        console.log("Event send!")
+        res.write( JSON.stringify( realData ));
+        console.log("Response Writed");
+      }
+       res.end();
+       console.log("Response End")
+      });
+    }});
     
     
   }
